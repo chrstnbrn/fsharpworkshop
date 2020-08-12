0de8dec8c1bcc003dd1169bec747f5cb4a897b7f
Christina Braun <11138584+chrstnbrn@users.noreply.github.com>
2020-08-12 14:32:28 +0200

    Exercises Module 4

--- Module4/Application/Services.fs
+++ Module4/Application/Services.fs
@@ -1,1 +1,18 @@
 ﻿namespace Services
+
+open System
+
+type CustomerService() =
+    member this.UpgradeCustomer =
+        Functions.getCustomer >> Functions.upgradeCustomer
+
+    member this.GetCustomerInfo customer =
+        let isAdult = Functions.isAdult customer
+        let alert = Functions.getAlert customer
+        sprintf
+            "Id: %i, IsVip: %b, Credit: %.2f, IsAdult: %b, Alert: %s"
+            customer.Id
+            customer.IsVip
+            customer.Credit
+            isAdult
+            alert
